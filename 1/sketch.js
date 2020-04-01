35a2d3e680d6a29fec3984bfba82ed95d84d7100
audreyannen <60931133+audreyannen@users.noreply.github.com>
2020-04-01 16:22:18 +0200

    first sketch

--- 1/sketch.js
+++ 1/sketch.js
@@ -1,21 +1,23 @@
 function setup() {
-  let navWidth = document.getElementById("nav").offsetWidth;
-  let canvas = createCanvas(windowWidth - navWidth, windowHeight);
-  canvas.parent('canvas');
-  background(255);
+    let navWidth = document.getElementById("nav").offsetWidth;
+    let canvas = createCanvas(windowWidth - navWidth, windowHeight);
+    canvas.parent('canvas');
+    background(255);
 }
 
 function draw() {
-  let x = 10;
-  fill(0);
-  while (x < width) {
-    ellipse(x, height / 2, 10, 10);
-    x = x + 30;
-  }
+    let x = 10;
+    stroke(50);
+    fill(0);
+    for (let x = 0; x < width; x = x + 10) {
+        for (let y = 0; y < height; y = y + 10)
+            line(x, y, width - 20, height - 20);
+    }
+
 }
 
 function windowResized() {
-  let navWidth = document.getElementById("nav").offsetWidth;
-  resizeCanvas(windowWidth - navWidth, windowHeight);
-  background(255);
+    let navWidth = document.getElementById("nav").offsetWidth;
+    resizeCanvas(windowWidth - navWidth, windowHeight);
+    background(255);
 }
